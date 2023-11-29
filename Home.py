a01738199b50f3f744b0b3ba3143406eae9a0584
uyilucky1 <uyilucky11@gmail.com>
2023-11-30 00:16:57 +0100

    Added_topics_file

--- Home.py
+++ Home.py
@@ -6,7 +6,7 @@ st.set_page_config(layout='wide')
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("Portfolio/images/luck.jpg")
+    st.image("ProfileApp/Portfolio/images/luck.jpg")
 
 with col2:
     st.title("Lucky Uyi")
@@ -29,14 +29,14 @@ st.write(content1)
 
 col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
-df = pd.read_csv('Portfolio/data.csv', sep=';')
+df = pd.read_csv('ProfileApp/Portfolio/data.csv', sep=';')
 
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row['title'])
         st.write(row['description'])
-        st.image('Portfolio/images/' + row['image'])
+        st.image('ProfileApp/Portfolio/images/' + row['image'])
         st.write(f'[Source Code]({row["url"]})')
 
 
@@ -46,7 +46,7 @@ with col4:
     for index, row in df[10:].iterrows():
         st.header(row['title'])
         st.write(row['description'])
-        st.image('Portfolio/images/' + row['image'])
+        st.image('ProfileApp/Portfolio/images/' + row['image'])
         st.write(f'[Source Code]({row["url"]})')
 
 
