5d7fb26b8122da6f5237fc66db23ca888b5d28c1
uyilucky1 <uyilucky11@gmail.com>
2023-11-30 00:25:06 +0100

    DAY 24:sent an email using the form

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,9 +1,13 @@
 import streamlit as st
 from send_email import send_mail
+import pandas as pd
+
+df = pd.read_csv('Portfolio/topics.csv')
 
 st.header('Contact Me')
 with st.form(key='email_forms'):
     user_email = st.text_input('Your Email Address')
+    option =
     raw_message = st.text_area('Your message')
     message = f"""\
 Subject:New email Alert {user_email}
