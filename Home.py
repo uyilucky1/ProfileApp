092a12ea3bc58aabc6373ad8fc22030dd7975c11
Lucky <94918609+uyilucky1@users.noreply.github.com>
2023-12-04 11:13:14 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,7 +7,7 @@ st.set_page_config(layout='wide')
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("ProfileApp/Portfolio/images/luck.jpg")
+    st.image("Portfolio/images/luck.jpg")
 
 with col2:
     st.title("Lucky Uyi")
